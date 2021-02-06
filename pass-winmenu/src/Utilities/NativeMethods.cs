88a9882d23aebe210e1385621158256b7f78eab2
Johan Geluk <johan@geluk.io>
2021-02-06 16:44:37 +0100

    WIP: Generate keyboard input by directly calling SendInput

--- pass-winmenu/src/Utilities/NativeMethods.cs
+++ pass-winmenu/src/Utilities/NativeMethods.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
@@ -18,10 +18,50 @@ namespace PassWinmenu.Utilities
 		[DllImport("user32.dll", SetLastError = true)]
 		public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
+		[DllImport("user32.dll", SetLastError = true)]
+		public static extern uint SendInput(uint nInputs,
+			[MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs,
+		 int cbSize);
+
 		public static Process GetWindowProcess(IntPtr hWnd)
 		{
 			GetWindowThreadProcessId(hWnd, out uint pid);
 			return Process.GetProcessById((int)pid);
 		}
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct INPUT
+	{
+		internal INPUT_TYPE Type;
+		internal KEYBDINPUT Data;
+	}
+
+	internal enum INPUT_TYPE : uint
+	{
+		INPUT_MOUSE = 0,
+		INPUT_KEYBOARD = 1,
+		INPUT_HARDWARE = 2
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct KEYBDINPUT
+	{
+		internal ushort KeyCode;
+		internal ushort ScanCode;
+		internal KEYEVENTF Flags;
+		internal uint Time;
+		internal IntPtr ExtraInfo;
+		private uint padding;
+		private uint padding_;
+	}
+
+	[Flags]
+	internal enum KEYEVENTF : uint
+	{
+		EXTENDEDKEY = 0x0001,
+		KEYUP = 0x0002,
+		SCANCODE = 0x0008,
+		UNICODE = 0x0004
 	}
 }
--- pass-winmenu/src/WinApi/KeyboardEmulator.cs
+++ pass-winmenu/src/WinApi/KeyboardEmulator.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using PassWinmenu.Utilities;
 
 namespace PassWinmenu.WinApi
 {
@@ -16,8 +20,54 @@ namespace PassWinmenu.WinApi
 		/// If true, inserts a space after every dead key in order to prevent it from being combined with the next character.</param>
 		internal static void EnterText(string text, bool escapeDeadKeys)
 		{
-			var escaped = escapeGenerator.Escape(text, escapeDeadKeys);
-			SendKeys.SendWait(escaped);
+			var inputs = new List<INPUT>();
+			foreach (var ch in text)
+			{
+				ushort scanCode = ch;
+				var down = new INPUT
+				{
+					Type = INPUT_TYPE.INPUT_KEYBOARD,
+					Data = new KEYBDINPUT
+					{
+						KeyCode = 0,
+						ScanCode = scanCode,
+						Flags = KEYEVENTF.UNICODE,
+						Time = 0,
+						ExtraInfo = IntPtr.Zero
+					}
+				};
+				var up = new INPUT
+				{
+					Type = INPUT_TYPE.INPUT_KEYBOARD,
+					Data = new KEYBDINPUT
+					{
+						KeyCode = 0,
+						ScanCode = scanCode,
+						Flags = KEYEVENTF.UNICODE | KEYEVENTF.KEYUP,
+						Time = 0,
+						ExtraInfo = IntPtr.Zero
+					}
+				};
+				// Handle extended keys:
+				// If the scan code is preceded by a prefix byte that has the value 0xE0 (224),
+				// we need to include the EXTENDEDKEY flag in the Flags property.
+				if ((scanCode & 0xFF00) == 0xE000)
+				{
+					down.Data.Flags |= KEYEVENTF.EXTENDEDKEY;
+					up.Data.Flags |= KEYEVENTF.EXTENDEDKEY;
+				}
+
+				inputs.Add(down);
+				inputs.Add(up);
+			}
+
+			var size = Marshal.SizeOf(typeof(INPUT));
+			var success = NativeMethods.SendInput((uint)inputs.Count, inputs.ToArray(), size);
+			if (success != inputs.Count)
+			{
+				var exc = Marshal.GetExceptionForHR(Marshal.GetHRForLastWin32Error());
+				throw exc;
+			}
 		}
 
 		internal static void EnterRawText(string text)
